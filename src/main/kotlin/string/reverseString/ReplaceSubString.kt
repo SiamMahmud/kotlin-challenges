81ab652d6d11a1f0f3b0dded1cd9e1c447aaf8c5
SiamMahmud <rakibsiam421@gmail.com>
2025-06-03 01:53:54 +0800

    Replace Substring with another String

---
+++ src/main/kotlin/string/reverseString/ReplaceSubString.kt
@@ -0,0 +1,13 @@
+package string.reverseString
+
+/**
+ *Replace Substring
+ * Input: String: "I love Java" Replace "Java" with "Kotlin"
+ * Output: "I love Kotlin"
+ */
+
+fun replaceString(str: String, oldStr: String, newStr: String): String{
+
+    return str.replace(oldStr,newStr)
+
+}
