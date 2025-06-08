c4ac3ca3b47062c354880f3947daecef6eec39f6
SiamMahmud <rakibsiam421@gmail.com>
2025-06-08 21:40:40 +0800

    Sorting an Array

---
+++ src/main/kotlin/array/SortArray.kt
@@ -0,0 +1,11 @@
+package com.rakib.array
+
+/**
+ *Sort Array
+ *Input: [3, 1, 4, 2]
+ *Output: [1, 2, 3, 4]
+ */
+
+fun sortArray(numbers: Array<Int>): Array<Int> {
+    return numbers.sortedArray()
+}
